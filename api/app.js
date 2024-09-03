a8e5be56033be02f5b8a0f0fc0b29ed45fece386
Umut Erdoğan <umuterdogan.imyo24@iste.edu.tr>
2024-09-03 03:08:08 +0300

    .

--- api/app.js
+++ api/app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  console.log("Ben app.js dosyasındayım");
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
