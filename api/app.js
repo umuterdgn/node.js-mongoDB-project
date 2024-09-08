c4d488cd5bebe1f7281d38e31ed7c97555706392
Umut Erdoğan <umuterdogan.imyo24@iste.edu.tr>
2024-09-08 22:03:49 +0300

    g

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   console.log("Ben app.js dosyasındayım");
-  next();
+  next();      
 });
 
 app.use('/', indexRouter);
